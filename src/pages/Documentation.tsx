30c9fb01d930c2560142e26624252a4bfd971f29
Fr0xty <ongjiae8@gmail.com>
2022-07-27 18:49:52 +0800

    added navbar into docs page

--- src/pages/Documentation.tsx
+++ src/pages/Documentation.tsx
@@ -1,7 +1,12 @@
+import Navbar from '../components/Navbar';
 import '../styles/Documentation.scss';
 
 const Documentation = () => {
-    return <div className="documentation"></div>;
+    return (
+        <div className="documentation">
+            <Navbar currentPage="docs" />
+        </div>
+    );
 };
 
 export default Documentation;
